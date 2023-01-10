5a88fd9dbdc88d962cf979b8fbf28961c1e0bf77
Robert Connolly <robertoconnolly100@gmail.com>
2023-01-10 13:52:47 +0100

    Final chat rsa

--- CHAT_RSA/main.py
+++ CHAT_RSA/main.py
@@ -2,14 +2,24 @@ import socket
 import threading
 import rsa
 
+#Creando key publica y privada
 mi_key_pub, mi_key_priv = rsa.newkeys(1024)
-su_key_pub = None
+su_key_pub = None # No sabemos su key publica
+IPV4 = None # Esto es la variable IP que se utilizara o para hostear o para conectarse 
 
-opcion = int(input("Quieres ser host? pon 1 o cliente pon 2"))
+#Menu
+opcion = int(input("Quieres ser host? pon 1 o cliente pon 2 "))
+#Seleciona la IP
+if opcion == 1:
+    IPV4 = input("Dame tu IPV4 de esta maquina para hostear: ") #Eliges la ip que quieres utilizar para hostear
+    #Haz ifconfig en linux para saber la ip de tu tarjeta de red o ipconfig en cmd en windows.
+elif opcion == 2:
+    IPV4 = input("Dame la IP de la maquina que te quieres conectar dentro de tu red: ")
+    #Aqui pones la IP de la maquina que esta hosteando
 
 if opcion == 1:
     socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket_servidor.bind(("192.168.1.101", 5000))
+    socket_servidor.bind((IPV4, 5000))
 
     socket_servidor.listen()
 
@@ -20,7 +30,7 @@ if opcion == 1:
     su_key_pub = rsa.PublicKey.load_pkcs1(cliente.recv(1024))
 elif opcion == 2:
     cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    cliente.connect(("192.168.1.101", 5000))
+    cliente.connect((IPV4, 5000))
     #Recibimos primero la key publica del host
     su_key_pub = rsa.PublicKey.load_pkcs1(cliente.recv(1024))
     #Enviamos nuestra key publica al host en el estandar de llaves PKCS1 con formato PEM:
@@ -38,11 +48,11 @@ def Enviar(client):
 
 def Recibir(client):
     while True:
-        print("El con IP:puerto: " + str(client.getpeername()) +  rsa.decrypt(client.recv(1024), mi_key_priv).decode("utf-8"))
+        print("El: " +  rsa.decrypt(client.recv(1024), mi_key_priv).decode("utf-8"))
 
 
-hilo1 = threading.Thread(target=Enviar, args=(cliente,))
-hilo2 = threading.Thread(target=Recibir, args=(cliente,))
+hilo1 = threading.Thread(target=Enviar, args=(cliente,)) # Creamos hilo para enviar
+hilo2 = threading.Thread(target=Recibir, args=(cliente,)) # Creamos hilo para recibir
 
 hilo1.start()
 hilo2.start()
